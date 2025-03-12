d500ca49af09492ba2734ff403384cd17acd0b11
AkashAkter <akash31akter@gmail.com>
2025-03-12 12:55:39 +0600

    created user schema

--- src/app/User.ts
+++ src/app/User.ts
@@ -12,23 +12,51 @@ const messageSchema: Schema<Message> = new Schema({
 });
 
 export interface User extends Document {
-    username: string;
-    email: string;
-    password: string;
-    verifyCode: string;
-    verifyCodeExpires: Date;
-    isAcceptingMessages: boolean;
-    messages: Message[];
-  }
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpires: Date;
+  isVerified: boolean;
+  isAcceptingMessages: boolean;
+  messages: Message[];
+}
 
-  const userSchema: Schema<User> = new Schema({
-    username: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    verifyCode: { type: String, required: true },
-    verifyCodeExpires: { type: Date, required: true },
-    isAcceptingMessages: { type: Boolean, required: true, default: true },
-    messages: [messageSchema],
-  });
-
-
+const userSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [
+      /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
+      "Please fill a valid email address",
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "Verify code is required"],
+  },
+  verifyCodeExpires: {
+    type: Date,
+    required: [true, "Verify code expiration date is required"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessages: {
+    type: Boolean,
+    default: true,
+  },
+  messages: [messageSchema],
+});
