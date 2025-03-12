b0e996253c0e6b3efd6e2adc890fbb5210e35be9
AkashAkter <akash31akter@gmail.com>
2025-03-12 12:48:27 +0600

    created user schema

--- src/app/User.ts
+++ src/app/User.ts
@@ -10,3 +10,25 @@ const messageSchema: Schema<Message> = new Schema({
   content: { type: String, required: true },
   createdAt: { type: Date, required: true, default: Date.now },
 });
+
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpires: Date;
+    isAcceptingMessages: boolean;
+    messages: Message[];
+  }
+
+  const userSchema: Schema<User> = new Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    verifyCode: { type: String, required: true },
+    verifyCodeExpires: { type: Date, required: true },
+    isAcceptingMessages: { type: Boolean, required: true, default: true },
+    messages: [messageSchema],
+  });
+
+
